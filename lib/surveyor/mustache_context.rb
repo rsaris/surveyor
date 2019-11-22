05650978f92f911462e63f7121eb301fe523baaf
Jeff Baxendale <jeffbax@gmail.com>
2019-11-22 13:44:57 -0500

    CHORE easier to debug

--- lib/surveyor/mustache_context.rb
+++ lib/surveyor/mustache_context.rb
@@ -4,9 +4,12 @@ module Surveyor
     
     def in_context(text, context=nil)
       case context
-      when NilClass then text
-      when Hash     then Mustache.render(text, context)
-      else               context.render(text)
+      when NilClass
+        text
+      when Hash
+        Mustache.render(text, context)
+      else
+        context.render(text)
       end
     end
   end
